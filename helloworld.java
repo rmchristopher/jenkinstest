58c8e3b43d30f20020f1d1ed925ff108d631fd52
rmchristopher <39762807+rmchristopher@users.noreply.github.com>
2018-05-30 11:43:31 +0000

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,7 +1,7 @@
 public class helloworld
 {
   public static void main(String[] args) {
-        System.out.println("Woo r ooooo");
+        System.out.println("I am a CSK fan!");
  //creating object
   }
 }
